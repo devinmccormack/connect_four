d06e3ab76bb1cdc5c9e4d8460edce77890cf0d35
Devin McCormack <devin.mccormack@getpowerpay.com>
2024-09-18 12:20:45 -0400

    Add method skeletons for board.rb

---
+++ lib/board.rb
@@ -0,0 +1,15 @@
+class Board
+  attr_reader :grid
+
+  def initialize
+    @grid = Array.new(6) { Array.new(7) }
+  end
+
+  def drop_piece
+    # code
+  end
+
+  def check_winner
+    # code
+  end
+end
