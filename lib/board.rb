eae7cc202dc6ce0f0902d1c91745fc038f685639
Devin McCormack <devin.mccormack@getpowerpay.com>
2024-09-18 15:48:18 -0400

    Build Board and Game classes to function and pass all tests

--- lib/board.rb
+++ lib/board.rb
@@ -12,6 +12,7 @@ class Board
     return false if row.nil?  # Column is full
 
     @grid[row][column] = piece
+    true
   end
 
   def check_winner(piece)
@@ -25,28 +26,22 @@ class Board
   end
 
   def lowest_available_row(column)
-    (0..5).reverse_each do |row|  # Start from the bottom row (5) to the top row (0)
+    (0..5).reverse_each do |row|
       return row if @grid[row][column].nil?
     end
     nil  # Return nil if the column is full
   end
 
   def horizontal_win?(piece)
-    @grid.each do |row|
-      row.each_cons(4) do |consecutive_pieces|
-        return true if consecutive_pieces.all? { |cell| cell == piece }
-      end
+    @grid.any? do |row|
+      row.each_cons(4).any? { |consecutive_pieces| consecutive_pieces.all? { |cell| cell == piece } }
     end
-    false
   end
 
   def vertical_win?(piece)
-    @grid.transpose.each do |column|
-      column.each_cons(4) do |consecutive_pieces|
-        return true if consecutive_pieces.all? { |cell| cell == piece }
-      end
+    @grid.transpose.any? do |column|
+      column.each_cons(4).any? { |consecutive_pieces| consecutive_pieces.all? { |cell| cell == piece } }
     end
-    false
   end
 
   def diagonal_win?(piece)
@@ -54,20 +49,18 @@ class Board
   end
 
   def diagonal_win_left_to_right?(piece)
-    (0..2).each do |row|
-      (0..3).each do |col|
-        return true if (0..3).all? { |i| @grid[row + i][col + i] == piece }
+    (0..2).any? do |row|
+      (0..3).any? do |col|
+        (0..3).all? { |i| @grid[row + i][col + i] == piece }
       end
     end
-    false
   end
 
   def diagonal_win_right_to_left?(piece)
-    (0..2).each do |row|
-      (3..6).each do |col|
-        return true if (0..3).all? { |i| @grid[row + i][col - i] == piece }
+    (0..2).any? do |row|
+      (3..6).any? do |col|
+        (0..3).all? { |i| @grid[row + i][col - i] == piece }
       end
     end
-    false
   end
 end
--- lib/game.rb
+++ lib/game.rb
@@ -1,27 +1,83 @@
-class Game
-  attr_reader :current_player
+require './lib/board'
 
+class Game
   def initialize
     @board = Board.new
     @current_player = 'X'
   end
 
   def play(column)
-    # Check if the move is valid (i.e., the column isn't full)
-    if @board.grid[0][column] # Top row has something, column is full
-      return false
+    if @board.drop_piece(column, @current_player)
+      if @board.check_winner(@current_player)
+        display_message("Player #{@current_player} wins!")
+        return true  # End the game
+      else
+        switch_player
+        false  # Continue the game
+      end
     else
-      # Drop the piece
-      @board.drop_piece(column, @current_player)
-      
-      # Switch player
-      switch_player
+      display_message("Column is full or invalid. Try another one.")
+      false  # Continue the game
     end
+  end
+
+  def start
+    display_message("Welcome to Connect Four!")
+    display_board
+
+    game_loop
   end
 
   private
 
+  def game_loop
+    loop do
+      display_message("Player #{@current_player}, enter a column number (0-6) to drop your piece:")
+      column = get_player_input
+
+      if column.nil?
+        display_message("Invalid input. Please enter a number between 0 and 6.")
+        next
+      end
+
+      if play(column)
+        display_board
+        break  # End the game
+      else
+        display_board
+      end
+    end
+  end
+
+  def get_player_input
+    input = gets.chomp
+    return nil unless input.match?(/\A\d+\z/)
+
+    column = input.to_i
+    (0..6).include?(column) ? column : nil
+  end
+
+  def display_board
+    @board.grid.each do |row|
+      row.each do |cell|
+        print cell.nil? ? '.' : cell
+        print ' '
+      end
+      puts
+    end
+    puts "0 1 2 3 4 5 6"  # Display column numbers for player reference
+  end
+
+  def display_message(message)
+    puts message
+  end
+
   def switch_player
     @current_player = @current_player == 'X' ? 'O' : 'X'
   end
+end
+
+# Run the game when this file is executed
+if __FILE__ == $0
+  Game.new.start
 end
