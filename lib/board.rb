07917f8ae5100e66eaf970f40333f66c84cc96e6
Devin McCormack <devin.mccormack@getpowerpay.com>
2024-09-18 12:33:06 -0400

    Add helper methods and code for check_winner method

--- lib/board.rb
+++ lib/board.rb
@@ -14,8 +14,8 @@ class Board
     @grid[row][column] = piece
   end
 
-  def check_winner
-    # code
+  def check_winner(piece)
+    horizontal_win?(piece) || vertical_win?(piece) || diagonal_win?(piece)
   end
 
   private
@@ -29,5 +29,45 @@ class Board
       return row if @grid[row][column].nil?
     end
     nil  # Return nil if the column is full
+  end
+
+  def horizontal_win?(piece)
+    @grid.each do |row|
+      row.each_cons(4) do |consecutive_pieces|
+        return true if consecutive_pieces.all? { |cell| cell == piece }
+      end
+    end
+    false
+  end
+
+  def vertical_win?(piece)
+    @grid.transpose.each do |column|
+      column.each_cons(4) do |consecutive_pieces|
+        return true if consecutive_pieces.all? { |cell| cell == piece }
+      end
+    end
+    false
+  end
+
+  def diagonal_win?(piece)
+    diagonal_win_left_to_right?(piece) || diagonal_win_right_to_left?(piece)
+  end
+
+  def diagonal_win_left_to_right?(piece)
+    (0..2).each do |row|
+      (0..3).each do |col|
+        return true if (0..3).all? { |i| @grid[row + i][col + i] == piece }
+      end
+    end
+    false
+  end
+
+  def diagonal_win_right_to_left?(piece)
+    (0..2).each do |row|
+      (3..6).each do |col|
+        return true if (0..3).all? { |i| @grid[row + i][col - i] == piece }
+      end
+    end
+    false
   end
 end
