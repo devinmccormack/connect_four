03e8006bf7d3f5a1bc973fe58fda88361c46509e
Devin McCormack <devin.mccormack@getpowerpay.com>
2024-09-18 12:29:03 -0400

    Add helper methods for drop_piece

--- lib/board.rb
+++ lib/board.rb
@@ -5,11 +5,29 @@ class Board
     @grid = Array.new(6) { Array.new(7) }
   end
 
-  def drop_piece
-    # code
+  def drop_piece(column, piece)
+    return false unless valid_column?(column)
+
+    row = lowest_available_row(column)
+    return false if row.nil?  # Column is full
+
+    @grid[row][column] = piece
   end
 
   def check_winner
     # code
+  end
+
+  private
+
+  def valid_column?(column)
+    column.between?(0, 6)
+  end
+
+  def lowest_available_row(column)
+    (0..5).reverse_each do |row|  # Start from the bottom row (5) to the top row (0)
+      return row if @grid[row][column].nil?
+    end
+    nil  # Return nil if the column is full
   end
 end
